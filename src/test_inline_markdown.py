03a1a9ef112c0c6ea7c1ecb42df1a57165789cb4
toddswift <todd.swift@gmail.com>
2025-07-30 14:07:13 -0400

    additional unit tests for links

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -123,6 +123,7 @@ class TestExtractMarkdownImages(unittest.TestCase):
             matches
         )
     
+    # this test failed unit tests
     #def test_invalid_syntax(self):
     #    matches = extract_markdown_images("![alt text(url) or [alt](url) or !(alt)[url]")
     #    self.assertListEqual([], matches)
@@ -137,12 +138,70 @@ class TestExtractMarkdownImages(unittest.TestCase):
         matches = extract_markdown_images("  ![  alt text  ](  url3.gif  )  ")
         self.assertListEqual([("  alt text  ", "  url3.gif  ")], matches)
     
+    # this test failed unit tests
     #def test_nested_brackets(self):
     #    matches = extract_markdown_images("![alt [nested] text](url4.jpg)")
     #    self.assertListEqual([("alt [nested] text", "url4.jpg")], matches)
 
-#class TestExtractMarkdownLinks(unittest.TestCase):
-    
+class TestExtractMarkdownLinks(unittest.TestCase):
+    def test_single_link(self):
+        matches = extract_markdown_links(
+            "This is text with a [link](https://example.com)"
+        )
+        self.assertListEqual([("link", "https://example.com")], matches)
+
+    def test_multiple_links(self):
+        matches = extract_markdown_links(
+            "Text [alt1](url1.jpg) more text [alt2](url2.png)"
+        )
+        self.assertListEqual([("alt1", "url1.jpg"), ("alt2", "url2.png")], matches)
+
+    def test_empty_string(self):
+        matches = extract_markdown_links("")
+        self.assertListEqual([], matches)
+
+    def test_no_links(self):
+        matches = extract_markdown_links("This is plain text without links")
+        self.assertListEqual([], matches)
+
+    def test_empty_anchor(self):
+        matches = extract_markdown_links("[](/page.html)")
+        self.assertListEqual([("", "/page.html")], matches)
+
+    def test_empty_url(self):
+        matches = extract_markdown_links("[anchor]()")
+        self.assertListEqual([("anchor", "")], matches)
+
+    def test_special_characters(self):
+        matches = extract_markdown_links(
+            "[anchor with spaces & special chars!](http://example.com/page with spaces.html?query=1)"
+        )
+        self.assertListEqual(
+            [("anchor with spaces & special chars!", "http://example.com/page with spaces.html?query=1")],
+            matches
+        )
+
+    def test_invalid_syntax(self):
+        matches = extract_markdown_links("[alt text(url) or [alt url) or (alt)[url]")
+        self.assertListEqual([], matches)
+
+    def test_mixed_valid_invalid(self):
+        matches = extract_markdown_links(
+            "[valid](url1.jpg) invalid [alt](url2.png) [] [alt](url)"
+        )
+        self.assertListEqual([("valid", "url1.jpg"), ("alt", "url2.png"), ("alt", "url")], matches)
+
+    def test_whitespace(self):
+        matches = extract_markdown_links("  [  anchor text  ](  url3.html  )  ")
+        self.assertListEqual([("  anchor text  ", "  url3.html  ")], matches)
+
+    def test_nested_brackets(self):
+        matches = extract_markdown_links("[alt [nested] text](url4.jpg)")
+        self.assertListEqual([], matches)
+
+    def test_no_match_for_images(self):
+        matches = extract_markdown_links("![image](url.jpg)")
+        self.assertListEqual([], matches)
 
 if __name__ == "__main__":
     unittest.main()

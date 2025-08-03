a118209f218db297201424068475a5676bf92d0f
toddswift <todd.swift@gmail.com>
2025-08-03 13:51:34 -0400

    Configured to build site to github docs code complete

--- src/main.py
+++ src/main.py
@@ -1,26 +1,28 @@
 import os
 import shutil
-
+import sys
 from copystatic import copy_files_recursive
 from gencontent import generate_pages_recursive
 
 
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_docs = "./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
 
 
 def main():
-    print("Deleting public directory...")
-    if os.path.exists(dir_path_public):
-        shutil.rmtree(dir_path_public)
+    basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
 
-    print("Copying static files to public directory...")
-    copy_files_recursive(dir_path_static, dir_path_public)
+    print("Deleting docs directory...")
+    if os.path.exists(dir_path_docs):
+        shutil.rmtree(dir_path_docs)
+
+    print("Copying static files to docs directory...")
+    copy_files_recursive(dir_path_static, dir_path_docs)
 
     print("Generating content...")
-    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
+    generate_pages_recursive(dir_path_content, template_path, dir_path_docs, basepath)
 
 
 main()
